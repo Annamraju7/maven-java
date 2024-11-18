8fd0c5285d4a14cbef0957f59c15a10ad896e363
shashank <shashankannamraju@gmail.com>
2024-11-18 13:26:35 +0530

    maven-java

---
+++ src/main/java/maven_project/a4/App.java
@@ -0,0 +1,17 @@
+package maven_project.a4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a=2;
+    	int b=4;
+    	System.out.println(a+b);
+    			
+        System.out.println( "Hello World!" );
+    }
+}
